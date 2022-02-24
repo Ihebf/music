b7a3c88bc4f3f464a2fb299ca6f1eb0c8d6c4b49
ihebf <iheb_farhat@hotmail.com>
2022-02-24 12:34:11 +0100

    update

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,12 @@ class ArtistController extends AbstractController
     /**
      * @Route("/artist", name="artist")
      */
-    public function index(): Response
+    public function index(UserRepository $repo): Response
     {
+        $list = $repo->findAll();
+
         return $this->render('artist/index.html.twig', [
-            'controller_name' => 'ArtistController',
+            'users' => $list,
         ]);
     }
 }
